12b158459d8bee2adca4162aa01533b82d4e7525
Muhammad Aqib <aqibwebdesign@gmail.com>
2025-06-24 23:53:21 -0700

    use variable without declaration

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 
 // document.getElementById("aqib").innerHTML = "Hello Aqib!";
 // document.getElementById("aqib").style.color = "blue";
+x = 5;
+y = 10;
+z = x + y;
 
+document.getElementById("aqib").innerHTML = "Hello Aqib! The sum of x and y is: " + z;
